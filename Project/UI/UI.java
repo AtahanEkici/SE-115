e12f661af6a74286455ef0785288660dd38d8092
Ata <laythese@gmail.com>
2020-09-13 13:28:42 +0300

    Add files via upload

--- Project/UI/UI.java
+++ Project/UI/UI.java
@@ -1,16 +1,10 @@
 package UI;
-import Bank.Account;
-import Bank.Bank;
-import static Utilities.Utility.readInfo;import java.awt.BorderLayout;import java.awt.Color;import java.awt.Desktop;import java.awt.FlowLayout;import java.awt.Font;import java.awt.event.ActionEvent;import java.awt.event.ActionListener;import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.net.URI;import java.util.ArrayList;import java.util.logging.Level;import java.util.logging.Logger;import javax.swing.BorderFactory;import javax.swing.ImageIcon;import javax.swing.JButton;import javax.swing.JFrame;import javax.swing.JLabel;import javax.swing.JMenu;import javax.swing.JMenuBar;import javax.swing.JMenuItem;import javax.swing.JOptionPane;import javax.swing.JPanel;import javax.swing.JTextArea;import javax.swing.JTextField;
-import javax.swing.JTextPane;import javax.swing.Timer;
+import Bank.Account;import Bank.Bank;import static Utilities.Utility.readInfo;import java.awt.BorderLayout;import java.awt.Color;import java.awt.Desktop;import java.awt.FlowLayout;import java.awt.Font;import java.awt.event.ActionEvent;import java.awt.event.ActionListener;import java.awt.event.KeyEvent;import java.awt.event.KeyListener;import java.awt.event.MouseEvent;import java.awt.event.MouseListener;import java.awt.event.WindowAdapter;import java.awt.event.WindowEvent;import java.net.URI;import java.util.ArrayList;import javax.swing.BorderFactory;import javax.swing.ImageIcon;import javax.swing.JButton;import javax.swing.JComboBox;import javax.swing.JFrame;import javax.swing.JLabel;import javax.swing.JMenu;import javax.swing.JMenuBar;import javax.swing.JMenuItem;import javax.swing.JOptionPane;import javax.swing.JPanel;import javax.swing.JTextField;import javax.swing.Timer;
 /**
  *
  * @author Atahan Ekici
  */
-public final class UI extends JFrame implements ActionListener,MouseListener
+public final class UI extends JFrame implements ActionListener,MouseListener,KeyListener
 {    
     private static UI single_instance = null;
     
@@ -18,27 +12,26 @@ public final class UI extends JFrame implements ActionListener,MouseListener
     {
         try
         {
+            Construct_Main_Frame();
             Construct_Credential_Frame();
-            //Construct_Main_Frame();
-            //setter();
-            //refresher();
-            //Construct_Exchange_Frame();
+            setter();
+            refresher();
+            Construct_Exchange_Frame();
         }
-        catch(Exception ex)
+        catch(Exception e)
         {
-             Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+              JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
         }   
     }  
     // ------------------------------ Swing Components ------------------------------ //
     private JFrame main,credential,exchange;
-    private JPanel jp1,jp2,jp3;
-    private JButton withdraw,deposit,button3,button4;
-    private JTextArea jta;
-    private JTextPane jtp;
+    private JPanel jp1,jp2;
+    private JButton withdraw,deposit,refresh,exc,SUBMIT;
     private JMenu aboutMenu;
     private JMenuBar mb;
     private JMenuItem jm_about,jm_github;
     private JLabel info,balanceLabel;
+    private JComboBox box;
     private Timer timer;
     // ------------------------------ Swing Components ------------------------------ //
     
@@ -46,7 +39,7 @@ public final class UI extends JFrame implements ActionListener,MouseListener
     
     private static final String Id = "admin";
     
-    private double a=-1, b=-1 , c=-1;
+    private double current_dollar=-1, current_euro=-1;
     
     // Bank and Account //
     private static final Bank bank = new Bank("Banka");
@@ -54,9 +47,151 @@ public final class UI extends JFrame implements ActionListener,MouseListener
     // Bank and Account //
     
     private double balance = a1.getBalance();
+    private double Euro_balance = 0;
+    private double Dollar_balance = 0;
     
     private double current_witdraw;
     private double current_deposit;
+    
+        public static UI getInstance()
+    {
+        if(single_instance == null)
+        {
+            single_instance = new UI();
+        }
+        return single_instance;    
+    }
+        
+        private void balancer()
+        {
+            balance = ((Euro_balance * current_euro) + (Dollar_balance * current_dollar));
+            balanceLabel = new JLabel("Your Balance is: "+balance+"");     
+        }
+    
+    private void Construct_Exchange_Frame()
+    {
+        exchange = new JFrame("Exchange Menu");
+        ImageIcon image = new ImageIcon(getClass().getResource("/Icons/exchange.png"));
+        exchange.setIconImage(image.getImage());
+        exchange.getContentPane().setBackground(PALE_BLACK);
+        exchange.setLayout(null);
+        exchange.setSize(300,200);
+        exchange.setResizable(false);
+        exchange.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        final String[] Currencies = {"Dollar", "Euro"};
+        
+        box = new JComboBox(Currencies);
+        box.setSelectedIndex(0);
+        box.setSize(100,20);
+        box.setLocation(100,0);
+        box.addActionListener(this);
+        
+        JButton selected = new JButton("Select");
+        selected.setSize(100,25);
+        selected.setLocation(100,50);
+        selected.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae) 
+            {
+                int selection = box.getSelectedIndex();
+                double offset_euro,offset_dolar;
+                Double Dolar;
+                Double Euro;
+                
+                switch (selection) 
+                {
+                    case 0:
+                        try
+                        {
+                            Dolar = Double.parseDouble(JOptionPane.showInputDialog(null," Please Specify Exchange Amount: "));
+                            
+                            if(Dolar < 0)
+                            {
+                                JOptionPane.showMessageDialog(null, "You can't enter negative values!!", "Error!!",JOptionPane.ERROR_MESSAGE);
+                            }
+                            else if(Dolar > balance)
+                            {
+                                JOptionPane.showMessageDialog(null, "The value you entered is exceeding your balance", "Error!!",JOptionPane.ERROR_MESSAGE);
+                            }
+                            else
+                            {   
+                            offset_dolar = Dolar * current_dollar;
+                            balance -= offset_dolar;
+                            Dollar_balance = Dolar;
+                            balancer();
+                            JOptionPane.showMessageDialog(null, "Successfully Exchanged","Success",JOptionPane.INFORMATION_MESSAGE);
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            if(e instanceof NullPointerException)
+                            {
+                                JOptionPane.showMessageDialog(null, "Nothing is entered", e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                            else if(e instanceof NumberFormatException)
+                            {
+                                JOptionPane.showMessageDialog(null, "You need to type double", e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                            else
+                            {
+                                JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                        
+                        break;
+                        
+                    case 1:
+                        try
+                        {
+                            Euro = Double.parseDouble(JOptionPane.showInputDialog(null," Please Specify Exchange Amount: ")); 
+                            
+                            if(Euro < 0)
+                            {
+                                JOptionPane.showMessageDialog(null, "You can't enter negative values!!", "Error!!",JOptionPane.ERROR_MESSAGE);
+                            }
+                            else if(Euro > balance)
+                            {
+                                JOptionPane.showMessageDialog(null, "The value you entered is exceeding your balance", "Error!!",JOptionPane.ERROR_MESSAGE);
+                            }
+                            else
+                            {   
+                            offset_euro = Euro * current_euro;
+                            balance -= offset_euro;
+                            Euro_balance = Euro;
+                            balancer();
+                            JOptionPane.showMessageDialog(null, "Successfully Exchanged","Success",JOptionPane.INFORMATION_MESSAGE);
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            if(e instanceof NullPointerException)
+                            {
+                                JOptionPane.showMessageDialog(null, "Nothing is entered", e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                            else if(e instanceof NumberFormatException)
+                            {
+                                JOptionPane.showMessageDialog(null, "You need to type double", e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                            else
+                            {
+                                JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), e.getClass().getCanonicalName(),JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                        break;
+                        
+                    default:
+                        JOptionPane.showMessageDialog(null, "Wrong Input Path", "Error!!",JOptionPane.ERROR_MESSAGE);
+                        break;
+                }
+            }  
+        });
+        exchange.add(box);
+        exchange.add(selected);
+        exchange.setLocationRelativeTo(main);
+        exchange.setVisible(false);
+    }
     
     private void Construct_Credential_Frame()
     {
@@ -68,6 +203,7 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         credential.setSize(300,300);
         credential.setResizable(false);
         credential.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        credential.addKeyListener(this);
         
         credential.addWindowListener(new WindowAdapter() 
 {
@@ -85,22 +221,26 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         JLabel label2 = new JLabel("ID:");  
         label2.setBounds(55,40,250,100);
         label2.setForeground(Color.WHITE);
+        label2.addKeyListener(this);
         
         JTextField ID = new JTextField();  
         ID.setBounds(90,82,150,20);  
+        ID.addKeyListener(this);
         
         JTextField Password = new JTextField();  
         Password.setBounds(90,120,150,20);
+        Password.addKeyListener(this);
         
         JLabel label3 = new JLabel("Pass:");  
         label3.setBounds(55,80,250,100);
         label3.setForeground(Color.WHITE);
+        label3.addKeyListener(this);
         
-        JButton hundred = new JButton();  
-        hundred.setText("SUBMIT");
-        hundred.setBounds(100,200,100,40);
-        hundred.setFocusable(false);
-        hundred.addActionListener((new ActionListener()
+        SUBMIT = new JButton();  
+        SUBMIT.setText("SUBMIT");
+        SUBMIT.setBounds(100,200,100,40);
+        SUBMIT.setFocusable(false);
+        SUBMIT.addActionListener((new ActionListener()
         {  
     @Override
     public void actionPerformed(ActionEvent e)
@@ -108,9 +248,10 @@ public final class UI extends JFrame implements ActionListener,MouseListener
             String id = ID.getText();
             String Pass = Password.getText();
             
-            if(id.matches(Id) && Pass.matches(Id))
+            if(id.equals(Id) && Pass.equals(Id))
             {
                 main.setVisible(true);
+                credential.dispose();
             }
             else
             {
@@ -122,7 +263,7 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         credential.add(label3);
         credential.add(ID);
         credential.add(Password);
-        credential.add(hundred);
+        credential.add(SUBMIT);
         credential.setLocationRelativeTo(null);
         credential.setVisible(true);
     }
@@ -153,7 +294,6 @@ public final class UI extends JFrame implements ActionListener,MouseListener
             jp2.setForeground(Color.WHITE);
             
             info = new JLabel("                 Euro:                 Dolar:                 Altın:                 ");
-            //info.setText(readInfo().toString());
             jp1.add(info,BorderLayout.CENTER);
             info.setForeground(Color.WHITE);
             
@@ -182,6 +322,18 @@ public final class UI extends JFrame implements ActionListener,MouseListener
             withdraw.setBackground(Color.WHITE);
             withdraw.setForeground(PALE_BLACK);
             
+            refresh = new JButton("Refresh"); 
+            refresh.setFocusable(false);
+            refresh.addActionListener(this);
+            refresh.setBackground(Color.WHITE);
+            refresh.setForeground(PALE_BLACK);
+            
+            exc = new JButton("Exchange"); 
+            exc.setFocusable(false);
+            exc.addActionListener(this);
+            exc.setBackground(Color.WHITE);
+            exc.setForeground(PALE_BLACK);
+            
             deposit = new JButton("Deposit");
             deposit.setFocusable(false);
             deposit.addActionListener(this);
@@ -190,19 +342,17 @@ public final class UI extends JFrame implements ActionListener,MouseListener
             
             jp2.add(withdraw);
             jp2.add(deposit);
+            jp2.add(exc);
+            jp2.add(refresh);
             
             main.add(balanceLabel,BorderLayout.CENTER);
             main.add(jp1,BorderLayout.NORTH);
             main.add(jp2,BorderLayout.CENTER);
             
             main.setLocationRelativeTo(null);
+            main.setVisible(false);
         }
-    
-    private void Construct_Exchange_Frame()
-    {
-        exchange = new JFrame("Exchange Menu");
-    }
-    
+
     private void refresh()
     {
         main.setVisible(false);
@@ -215,32 +365,24 @@ public final class UI extends JFrame implements ActionListener,MouseListener
     private void setter()
     {
         ArrayList<Double> list = readInfo();
-        
+        String text;
         double euro = list.get(0);
         double dolar = list.get(1);
-        double altın = list.get(2);
-        
-        String text = null;
-        
-        if(a == -1)
+
+        if(current_euro == -1)
         {
-            a = euro;
+           current_euro = euro;
         }
-        if(b == -1)
+        if(current_dollar == -1)
         {
-            b = dolar;
-        }
-        if(c == -1)
-        {
-            c = altın;
+           current_dollar  = dolar;
         }
         
-        
-        if(a < euro)
+        if(current_euro < euro)
         {
             text = "<html>Euro: <FONT COLOR=RED>" +euro+ "</FONT> ";
         }
-        else if(a == euro)
+        else if(current_euro == euro)
         {
             text = "<html>Euro: <FONT COLOR=YELLOW>" +euro+ "</FONT> ";
         }
@@ -250,11 +392,11 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         }
         
         
-        if(b < dolar)
+        if(current_dollar < dolar)
         {
             text += " Dolar: <FONT COLOR=RED>" +dolar+ "</FONT> ";
         }
-        else if(b == dolar)
+        else if(current_dollar == dolar)
         {
             text += " Dolar: <FONT COLOR=YELLOW>" +dolar+ "</FONT> ";
         }
@@ -262,23 +404,6 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         {
             text += " Dolar: <FONT COLOR=GREEN>" +dolar+ "</FONT> ";
         }
-        
-        
-        if(c < altın)
-        {
-            text += " Altın: <FONT COLOR=RED>" +altın+ "</FONT></html> ";
-            c = altın;
-        }
-        else if(c == altın)
-        {
-            text += " Altın: <FONT COLOR=YELLOW>" +altın+ "</FONT></html> ";
-            c = altın;
-        }
-        else
-        {
-            text += " Altın: <FONT COLOR=GREEN>" +altın+ "</FONT></html> ";
-            c = altın;
-        } 
         info.setText(text);
     }
     
@@ -288,18 +413,9 @@ public final class UI extends JFrame implements ActionListener,MouseListener
         {
             setter();
         });
-            timer.start();
+        timer.start();
     }
     
-    public static UI getInstance()
-    {
-        if(single_instance == null)
-        {
-            single_instance = new UI();
-        }
-            return single_instance;    
-    }
-
     @Override
     public void actionPerformed(ActionEvent Event) // Action Listener instructions //
     {
@@ -371,6 +487,17 @@ public final class UI extends JFrame implements ActionListener,MouseListener
                 } 
             }   
         }
+        else if(Event.getSource() == refresh)
+        {
+            refresh();
+            setter();
+        }
+        
+        else if(Event.getSource() == exc)
+        {
+            exchange.setVisible(true);
+        }
+        
         else if(Event.getSource() == jm_about)
         {
              try
@@ -416,6 +543,7 @@ public final class UI extends JFrame implements ActionListener,MouseListener
             aboutMenu.setSelected(false); // dispose hover effect //
         }
     } 
+    
     // NOT NEEDED //
     @Override
     public void mouseClicked(MouseEvent me){}
@@ -424,4 +552,20 @@ public final class UI extends JFrame implements ActionListener,MouseListener
     @Override
     public void mouseReleased(MouseEvent me){}
     // NOT NEEDED //
+
+    @Override
+    public void keyPressed(KeyEvent e) 
+    {
+        if (e.getKeyCode()== KeyEvent.VK_ENTER)
+        {
+            SUBMIT.doClick();
+        }    
+    }
+
+     // NOT NEEDED //
+    @Override
+    public void keyTyped(KeyEvent ke){}
+    @Override
+    public void keyReleased(KeyEvent ke){}
+     // NOT NEEDED //
 }
